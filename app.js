aa458e33fce0f89a8c756f3f9968c1065ee375ee
Vinh <vinhluong017@gmail.com>
2019-10-25 19:30:57 +0700

    display data from mongo

--- app.js
+++ app.js
@@ -3,14 +3,18 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var sesstion = require('express-session')
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var mongoose = require('mongoose')
+var passport = require('passport')
+var flash = require('connect-flash')
+
+
 var app = express();
 
 // mongoose.connect('localhost:27017/shopping')
-
+require('./config/passport')
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,6 +24,10 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(sesstion({secret: 'mysecret', resave: false, saveUninitialized: false}))
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session())
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,44 @@
 var express = require('express');
 var router = express.Router();
-var Product = require('../model/product')
-/* GET home page. */
-router.get('/', function(req, res, next) {
+var Mongo = require('mongodb').MongoClient;
+var passport = require('passport')
+require('../config/passport')
+var csrf = require('csurf')
+const url = "mongodb://localhost:27017/"
+var csrfProtection = csrf()
+router.use(csrfProtection)
 
-  var products = Product.find();
-  res.render('index', { title: 'Express', products: products });
+
+router.get('/', function (req, res, next) {
+  Mongo.connect(url, function (err, db) {
+    if (err) throw err;
+    var dbo = db.db("shopping");
+
+    dbo.collection("products").find({}).toArray(function(err, result) {
+      if (err) throw err;
+      var products = [];
+      for(let data of result){
+        products.push(data)
+      }
+      db.close();
+       res.render('index', {products: products});
+    });
+  }); 
 });
+
+router.get('/user/signup', function(req, res, next) {
+  res.render('user/signup', {csrfToken: req.csrfToken()})
+})
+
+router.post('/user/signup', passport.authenticate('local.signup', {
+  successRedirect: '/user/profile',
+  failureRedirect: '/user/signup',
+  failureFlash: true
+}))
+
+router.get('/user/profile', function(req, res, next) {
+  res.render('user/profile')
+})
+
 
 module.exports = router;
